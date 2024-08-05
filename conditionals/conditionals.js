2842d45321d274eb17177d59aa5ab229a49c314d
Elvin German <ElvinG13@nycstudents.net>
2024-08-05 17:54:57 +0000

    added stuff

--- conditionals/conditionals.js
+++ conditionals/conditionals.js
@@ -2,30 +2,30 @@
 10 == 3 // returns false
 5 == 5 // returns true
 6 == "6" // returns true
-// console.log('5 and 5 are equal:', 5 == 5);
-// console.log('10 and 3 are equal:', 10 == 3);
+console.log('5 and 5 are equal:', 5 == 5);
+console.log('10 and 3 are equal:', 10 == 3);
 
 // Not equal values
 10 != 3 // returns true
 5 != 5 // returns false
-// console.log('10 and 3 are not equal:', 10 != 3);
-// console.log('5 and 5 are not equal:', 5 != 5);
+ console.log('10 and 3 are not equal:', 10 != 3);
+ console.log('5 and 5 are not equal:', 5 != 5);
 
 // Less than
 10 < 3 // returns false
 5 < 5 // returns false
 4 < 5 // returns true
-// console.log('10 is less than 3:', 10 < 3);
-// console.log('5 is less than 5:', 5 < 5);
-// console.log('4 is less than 5:', 4 < 5);
+ console.log('10 is less than 3:', 10 < 3);
+ console.log('5 is less than 5:', 5 < 5);
+ console.log('4 is less than 5:', 4 < 5);
 
 // Greater than
 10 > 3 // returns true
 5 > 5 // returns false
 4 > 5 // returns false
-// console.log('10 is greater than 3:', 10 > 3);
-// console.log('5 is greater than 5:', 5 > 5);
-// console.log('4 is greater than 5:', 4 > 5);
+ console.log('10 is greater than 3:', 10 > 3);
+ console.log('5 is greater than 5:', 5 > 5);
+ console.log('4 is greater than 5:', 4 > 5);
 
 // Less than or Equal values
 10 <= 3 // returns false
@@ -39,25 +39,25 @@
 10 >= 3 // returns true
 5 >= 5 // returns true
 4 >= 5 // returns false
-// console.log('10 is greater than or equal to 3:', 10 >= 3);
-// console.log('5 is greater than or equal to 5:', 5 >= 5);
-// console.log('4 is greater than or equal to 5:', 4 >= 5);
+ console.log('10 is greater than or equal to 3:', 10 >= 3);
+ console.log('5 is greater than or equal to 5:', 5 >= 5);
+ console.log('4 is greater than or equal to 5:', 4 >= 5);
 
 // Equal values and type
 10 === 3 // returns false
 5 === 5 // returns true
 6 === "6" // returns false
-// console.log('5 and 5 are equal and of same type:', 5 === 5);
-// console.log('10 and 3 are equal and of same type:', 10 === 3);
-// console.log('6 and "6" are equal and of same type:', 6 === "6");
+ console.log('5 and 5 are equal and of same type:', 5 === 5);
+ console.log('10 and 3 are equal and of same type:', 10 === 3);
+ console.log('6 and "6" are equal and of same type:', 6 === "6");
 
 // Not equal values or type
 10 !== 3 // returns true
 5 !== 5 // returns false
 6 !== "6" // returns true
-// console.log('10 and 3 are not equal or not of same type:', 10 !== 3);
-// console.log('5 and 5 are not equal or not of same type:', 5 !== 5);
-// console.log('6 and "6" are not equal or not of same type:', 6 !== "6");
+ console.log('10 and 3 are not equal or not of same type:', 10 !== 3);
+ console.log('5 and 5 are not equal or not of same type:', 5 !== 5);
+ console.log('6 and "6" are not equal or not of same type:', 6 !== "6");
 
 // AND operator 
 let x = 4;
@@ -67,9 +67,9 @@ let z = 12;
 x < 5 && y > 5; // returns true because both conditions are true
 x > 5 && z > 10; // returns false because x > 5 is false
 y > 10 && z > 10; // returns false because y > 10 is false
-// console.log('x < 5 and y > 5:', x < 5 && y > 5);
-// console.log('x > 5 and z > 10:', x > 5 && z > 10);
-// console.log('y > 10 and z > 10:', y > 10 && z > 10);
+ console.log('x < 5 and y > 5:', x < 5 && y > 5);
+ console.log('x > 5 and z > 10:', x > 5 && z > 10);
+ console.log('y > 10 and z > 10:', y > 10 && z > 10);
 
 // OR operator
 let a = 3;
@@ -84,34 +84,46 @@ b > 5 || c < 8; // returns true because b > 5 is true
 // console.log('b > 5 or c < 8:', b > 5 || c < 8);
 
 //if statement 
-// let age = 25;
-// if (age >= 18) {
-//   console.log("You can vote!");
-// }
+ let age = 17;
+ if (age >= 18) {
+   console.log("You can vote!");
+ }
+ else{
+    console.log("You can't vote ):");
+ }
 
-//if else 
-// let number = 2
-// if(number > 0){
-//     console.log("number is positive")
-// }
-// else {
-//     console.log("number is not greater than 0")
-// }
 
-//if else if else
-// let number = 2
-// if(number > 0){
-//     console.log("number is positive")
-// }
-// else if(number == 0){
-//     console.log("number is equal to 0")
-// }
-// else {
-//     console.log("number is negative")
-// }
+ let number = 2
+ if(number > 0) {
+    console.log("number is positive");
+ }
+ else if(number == 0) {
+    console.log("number is 0");
+ }
+ else {
+    console.log("number is less than 0");
+ }
+
 
 // Write a conditional statement to find the largest of three numbers
 // Make 3 variables that prompts the user for a number
 // Use conditionals and comparisons to find which of the 3 is largest 
 // Console.log the result
 // Result: “x is the largest number”
+function getRandomInt(max) {
+   return Math.floor(Math.random() * max);
+ }
+
+
+let Abra = Math.floor(getRandomInt(3));
+let Kadabra = Math.floor(getRandomInt(3));
+let Alakazam = Math.floor(getRandomInt(3));
+if( Abra > Kadabra && Abra > Alakazam){
+   console.log("Abra" + " is the largest number");
+}
+else if( Kadabra > Abra && Kadabra> Alakazam){
+   console.log("Kadabra" + " is the largest number");
+}
+else if( Alakazam > Kadabra && Alakazam > Abra){
+   console.log("Alakazam" + " is the largest number");
+}
